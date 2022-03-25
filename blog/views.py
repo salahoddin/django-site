eda69ee98db6ff17cd1959d85f978da9764cc237
salahoddin <salahoddinsambutuan@gmail.com>
2022-03-25 23:58:40 +0800

    created database and fetced data from it

--- blog/views.py
+++ blog/views.py
@@ -1,59 +1,30 @@
-from datetime import date
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-all_posts = [
-    {
-        'slug': 'bike-in-the-mountains',
-        'image': 'mountains.jpg',
-        'author': 'Salahod-din',
-        'date': date(2018, 7, 21),
-        'title': 'Biking',
-        'excerpt': "Here's an amazing view taken when we we're biking! There's nothing like the views you get when you're biking in the mountain!",
-        'content': "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, et? Iure sed ea voluptatem nulla id provident eum voluptatibus ipsam quam vitae eaque, pariatur nesciunt velit unde aliquid esse laboriosam?"
-    },
-    {
-        'slug': 'learning-to-code',
-        'image': 'coding.png',
-        'author': 'Salahod-din',
-        'date': date(2019, 7, 21),
-        'title': 'Coding',
-        'excerpt': "I am learning python programming language and django a python framework for web development.",
-        'content': "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, et? Iure sed ea voluptatem nulla id provident eum voluptatibus ipsam quam vitae eaque, pariatur nesciunt velit unde aliquid esse laboriosam?"
-    },
-    {
-        'slug': 'hike-in-the-woods',
-        'image': 'woods.jpg',
-        'author': 'Salahod-din',
-        'date': date(2020, 7, 21),
-        'title': 'Hiking',
-        'excerpt': "Viewing the nice forest from here in Mindanao!",
-        'content': "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, et? Iure sed ea voluptatem nulla id provident eum voluptatibus ipsam quam vitae eaque, pariatur nesciunt velit unde aliquid esse laboriosam?"
-    },
-]
-
-
-def get_date(post):
-    return post['date']
 
 # Create your views here.
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by('-date')[:3]
+
     return render(request, 'blog/index.html', {
         'posts': latest_posts
     })
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by('-date')
+
     return render(request, 'blog/all-posts.html', {
         'all_posts': all_posts
     })
 
 
 def post_detail(request, slug):
-    post = next(post for post in all_posts if post['slug'] == slug)
+    post = get_object_or_404(Post, slug=slug)
+    # Post.objects.get(slug=slug) # above is a shortcut instead of using try/except
     return render(request, 'blog/post-detail.html', {
-        'post': post
+        'post': post,
+        'post_tags': post.tags.all()
     })
